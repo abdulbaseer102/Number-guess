36f296c3ffb6f461ac38587cab20ac1682a1e4ff
abdulbaseer102 <147842675+abdulbaseer102@users.noreply.github.com>
2024-03-20 00:28:15 +0500

    Number guess

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+type ansType = {
+  Userguess: number
+}
+const systemGenratedNo =Math.floor( Math.random() *10);
+
+
+const answers: ansType = await inquirer.prompt([
+  {
+    type: 'number',
+    name: 'Userguess',
+    message: ' Write Your Guess b/w 0 to 10:'
+  }
+])
+const {Userguess} = answers;
+console.log(Userguess,"Userguess", systemGenratedNo,"SYs");
+  
+if (Userguess === systemGenratedNo){
+ 
+  console.log('Omg Your Answer is correct\n You Win!');
+}
+else{
+  console.log('Please Try Again Batter Luck Next Time!');
+}
